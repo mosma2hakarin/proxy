0134d46aea3b527f0e9eea24384f8acb148d1bb3
mosma2hakarin <97221588+mosma2hakarin@users.noreply.github.com>
2023-10-12 16:40:46 +0200

    Update proxy.js

--- proxy.js
+++ proxy.js
@@ -5,7 +5,7 @@ const proxy = httpProxy.createProxyServer({});
 
 const server = http.createServer(function (req, res) {
   proxy.web(req, res, {
-    target: 'http://149.18.63.85:30120',
+    target: 'https://cfx.re/join/l6keoo',
   });
 });
 
