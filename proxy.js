af1ae194a0057a4fd7efe7fe2802b0b1db9c2d0b
mosma2hakarin <97221588+mosma2hakarin@users.noreply.github.com>
2023-10-12 16:45:48 +0200

    Update proxy.js

--- proxy.js
+++ proxy.js
@@ -1,12 +1,25 @@
-const http = require('http');
+const express = require('express');
 const httpProxy = require('http-proxy');
+const cors = require('cors'); // For handling CORS if needed
+
+const app = express();
+app.use(cors()); // Enable CORS if required
 
 const proxy = httpProxy.createProxyServer({});
+const target = process.env.TARGET_SERVER || 'http://149.18.63.85:30120';
 
-const server = http.createServer(function (req, res) {
-  proxy.web(req, res, {
-    target: 'https://cfx.re/join/l6keoo',
-  });
+// Error handling for the proxy
+proxy.on('error', (err, req, res) => {
+  console.error('Proxy error:', err);
+  res.status(500).send('Proxy error');
 });
 
-server.listen(process.env.PORT || 3000);
+// Proxy all requests to the target server
+app.all('/*', (req, res) => {
+  proxy.web(req, res, { target });
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log('Proxy server is running on port ' + port);
+});
